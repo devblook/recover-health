84b70f334b207809ccde7815d9fc91401e63fba6
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-12-29 21:49:40 -0500

    feat: convert commandexecutor class to a commandclass

--- src/main/java/me/bryang/recoverhealth/commands/RecoverHealthCommand.java
+++ src/main/java/me/bryang/recoverhealth/commands/RecoverHealthCommand.java
@@ -1,119 +1,83 @@
 package me.bryang.recoverhealth.commands;
 
-import me.bryang.recoverhealth.PluginService;
 import me.bryang.recoverhealth.manager.FileManager;
 import me.bryang.recoverhealth.utils.TextUtils;
+import me.fixeddev.commandflow.annotated.CommandClass;
+import me.fixeddev.commandflow.annotated.annotation.Command;
+import me.fixeddev.commandflow.annotated.annotation.OptArg;
+import me.fixeddev.commandflow.bukkit.annotation.Sender;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import team.unnamed.inject.InjectAll;
 
+import javax.inject.Named;
 import java.util.List;
 
-public class RecoverHealthCommand implements CommandExecutor {
 
-    private final PluginService pluginService;
+@Command(names = {"recoverhealth", "rh", "rhealth", ""})
+@InjectAll
+public class RecoverHealthCommand implements CommandClass {
 
-    private final FileManager configFile;
-    private final FileManager messagesFile;
 
-    public RecoverHealthCommand(PluginService pluginService) {
-        this.pluginService = pluginService;
+    private FileManager configFile;
 
-        this.configFile = pluginService.getFiles().getConfigFile();
-        this.messagesFile = pluginService.getFiles().getMessagesFile();
+    @Named("messages")
+    private FileManager messagesFile;
+
+
+    @Command(names = "")
+    public void mainSubCommand(@Sender Player sender) {
+        messagesFile.getStringList("command.help")
+                .forEach(sender::sendMessage);
+
     }
 
+    @Command(names = "give", permission = "recoverhealth.give")
+    public void reloadSubCommand(@Sender Player sender, @OptArg("1") int quantity) {
 
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        Material material = Material.getMaterial(configFile.getString("item.id").toUpperCase());
 
-        if (!(commandSender instanceof Player)) {
-            System.out.println(messagesFile.getString("error.console"));
-            return true;
+        if (material == null) {
+            Bukkit.getLogger().info("Unknown material!");
+            return;
         }
 
-        Player sender = (Player) commandSender;
+        ItemStack itemStack = new ItemStack(material);
 
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(configFile.getString("item.name"));
 
-        if (args.length < 1) {
-            messagesFile.getStringList("command.help")
-                    .forEach(sender::sendMessage);
-            return true;
+        if (!configFile.getStringList("item.lore").isEmpty()) {
+            List<String> lore = configFile.getStringList("item.lore");
+            itemMeta.setLore(lore);
         }
 
-        switch (args[0]) {
-            case "give":
+        itemStack.setItemMeta(itemMeta);
 
+        Inventory inventory = sender.getInventory();
+        int times = 0;
 
-                if (!sender.hasPermission("recoverhealth.give")) {
-                    sender.sendMessage(messagesFile.getString("error.no-permissions"));
-                    return true;
-                }
-
-                String quantity;
-                if (args.length < 2) {
-                    quantity = "1";
-                } else {
-                    quantity = args[1];
-                }
-                if (!TextUtils.isNumeric(quantity)) {
-                    sender.sendMessage(messagesFile.getString("error.unknown-number")
-                            .replace("%number%", quantity));
-                    return true;
-                }
-
-                Material material = Material.getMaterial(configFile.getString("item.id").toUpperCase());
-
-                if (material == null) {
-                    pluginService.getPlugin().getLogger().info("Unknown material!");
-                    return true;
-                }
-
-                ItemStack itemStack = new ItemStack(material);
-
-                ItemMeta itemMeta = itemStack.getItemMeta();
-                itemMeta.setDisplayName(configFile.getString("item.name"));
-
-                if (!configFile.getStringList("item.lore").isEmpty()) {
-                    List<String> lore = configFile.getStringList("item.lore");
-                    itemMeta.setLore(lore);
-                }
-
-                itemStack.setItemMeta(itemMeta);
-
-                Inventory inventory = sender.getInventory();
-                int times = 0;
-
-                while (times != Integer.parseInt(quantity)) {
-                    inventory.addItem(itemStack);
-                    times++;
-                }
-                sender.updateInventory();
-
-                sender.sendMessage(messagesFile.getString("command.give")
-                        .replace("%number%", quantity));
-                return true;
-            case "reload":
-
-                if (!sender.hasPermission("recoverhealth.reload")) {
-                    sender.sendMessage(messagesFile.getString("error.no-permissions"));
-                    return true;
-                }
-
-                configFile.reload();
-                messagesFile.reload();
-
-                sender.sendMessage(messagesFile.getString("command.reload"));
-                return true;
-            default:
-                sender.sendMessage(messagesFile.getString("error.unknown-args"));
-                return true;
+        while (times != quantity) {
+            inventory.addItem(itemStack);
+            times++;
         }
+        sender.updateInventory();
 
+        sender.sendMessage(messagesFile.getString("command.give")
+                .replace("%number%", String.valueOf(quantity)));
+
+    }
+
+    @Command(names = "reload", permission = "recoverhealth.reload")
+    public void reloadSubCommand(@Sender Player sender) {
+        configFile.reload();
+        messagesFile.reload();
+
+        sender.sendMessage(messagesFile.getString("command.reload"));
     }
 }
