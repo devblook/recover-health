2b018ff1185f4fd81bfc7fe0567f784ad57484f6
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-12-29 19:21:00 -0500

    feat: add service loader

--- src/main/java/me/bryangaming/recoverhealth/PluginService.java
+++ src/main/java/me/bryangaming/recoverhealth/PluginService.java
@@ -1,18 +1,14 @@
 package me.bryangaming.recoverhealth;
 
-import me.bryangaming.recoverhealth.loader.CommandsLoader;
-import me.bryangaming.recoverhealth.loader.EventsLoader;
-import me.bryangaming.recoverhealth.loader.FilesLoader;
-import me.bryangaming.recoverhealth.manager.SenderManager;
+import me.bryangaming.recoverhealth.services.CommandService;
+import me.bryangaming.recoverhealth.services.ListenerService;
+import me.bryangaming.recoverhealth.services.FileService;
+import me.bryangaming.recoverhealth.services.Service;
 
 public class PluginService {
 
     private final RecoverHealth recoverHealth;
-    private FilesLoader fileLoader;
-
-    private SenderManager senderManager;
-    private CommandsLoader commandsLoader;
-    private EventsLoader eventsLoader;
+    private FileService fileServices;
 
     public PluginService(RecoverHealth recoverHealth){
         this.recoverHealth = recoverHealth;
@@ -20,22 +16,28 @@ public class PluginService {
     }
 
     public void setup(){
-        fileLoader = new FilesLoader(this);
-        senderManager = new SenderManager(this);
-        commandsLoader = new CommandsLoader(this);
-        eventsLoader = new EventsLoader(this);
+
+        fileServices = new FileService(recoverHealth);
+
+        initServices(
+                fileServices,
+                new CommandService(this),
+                new ListenerService(this));
+    }
+
+    public void initServices(Service... services){
+        for (Service service : services){
+            service.init();
+        }
     }
 
     public RecoverHealth getPlugin() {
         return recoverHealth;
     }
 
-    public FilesLoader getFiles(){
-        return fileLoader;
+    public FileService getFiles(){
+        return fileServices;
     }
 
-    public SenderManager getSender(){
-        return senderManager;
-    }
 
 }
