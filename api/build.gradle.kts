980632d610cda2a003cc214983d8e5ff86242949
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2023-01-09 11:43:51 -0500

    refactor(gradle): remove unnecesary repositories and dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,17 +2,3 @@ plugins{
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("java-library")
 }
-
-repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://repo.unnamed.team/repository/unnamed-public/")
-    mavenLocal()
-}
-
-dependencies {
-    compileOnly ("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
-    implementation ("team.unnamed:inject:1.0.1")
-    implementation ("me.fixeddev:commandflow-universal:0.5.3")
-    implementation ("me.fixeddev:commandflow-bukkit:0.5.2")
-}
