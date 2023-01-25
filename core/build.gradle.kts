cc4680065de00fb472ab9c647368823713c988e2
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2023-01-24 20:28:02 -0500

    feat: add x-series dependencies, to use the sound class and configurate to use the file system

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -13,9 +13,12 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+
     implementation ("team.unnamed:inject:1.0.1")
     implementation ("me.fixeddev:commandflow-universal:0.5.3")
     implementation ("me.fixeddev:commandflow-bukkit:0.5.2")
+    implementation ("com.github.cryptomorin:xSeries:9.2.0") { isTransitive = false }
+    implementation ("org.spongepowered:configurate-yaml:4.0.0")
 }
 tasks {
     shadowJar {
@@ -23,6 +26,7 @@ tasks {
 
         relocate("me.fixeddev", "${project.group}.recoverhealth.libs.commandflow")
         relocate("team.unnamed.inject", "${project.group}.recoverhealth.libs.inject")
+        relocate("com.cryptomorin.xseries", "${project.group}.recoverhealth.libs.xseries")
 
     }
 }
