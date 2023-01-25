15bc48d13d34f5e771e8e9d43b6fdcddd6d0c0a6
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2023-01-24 20:24:05 -0500

    refactor: remove file manager constructor

--- core/src/main/java/me/bryang/recoverhealth/modules/FileModule.java
+++ core/src/main/java/me/bryang/recoverhealth/modules/FileModule.java
@@ -1,24 +1,25 @@
 package me.bryang.recoverhealth.modules;
 
+import me.bryang.recoverhealth.FileCreator;
 import me.bryang.recoverhealth.RecoverHealth;
-import me.bryang.recoverhealth.manager.FileManager;
 import team.unnamed.inject.AbstractModule;
+
+import javax.inject.Inject;
 
 public class FileModule extends AbstractModule {
 
-    private final RecoverHealth recoverHealth;
-
-    public FileModule(RecoverHealth recoverHealth) {
-        this.recoverHealth = recoverHealth;
-    }
+    @Inject
+    private RecoverHealth plugin;
 
     @Override
     public void configure() {
 
-        bind(FileManager.class)
-                .toInstance(new FileManager(recoverHealth, "config.yml"));
-        bind(FileManager.class)
+        bind(FileCreator.class)
+                .toInstance(new FileCreator("config"));
+        bind(FileCreator.class)
                 .named("messages")
-                .toInstance(new FileManager(recoverHealth, "messages.yml"));
+                .toInstance(new FileCreator("messages"));
+
+        plugin.getLogger().info("Files loaded!");
     }
 }
